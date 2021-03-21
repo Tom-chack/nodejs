1a7d8c9ff5498d1b6da72444eed754760b58dc9f
Tom-chack <tom.controlcenter@gmail.com>
2021-03-21 11:14:29 +0300

    Homework 16.5

---
+++ homework/16/homework5.js
@@ -0,0 +1,8 @@
+// 5.Modules պապակայում ստեղծել charOneSecondOutput մոդուլ charOneSecondOutput ֆունկցիայով
+// որը console-ում  արտածում է ֆունկցիային տրված բառի տառերը 1 վայրկյան պարբերությամբ:
+// Մոդուլը ներմուծել homework5.js ֆայլում և charOneSecondOutput ֆունկցիա կանչել
+// Ձեր անուն ազգանունից  կազմված բառակապակցության համար :
+
+const myModule = require('./modules/charOneSecondOutput');
+
+myModule.charOneSecondOutput('Artyom Chakhoyan');
