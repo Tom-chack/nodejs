ebfb9f6e4e29af90b7d4b428791cf41946d59e0f
Tom-chack <tom.controlcenter@gmail.com>
2021-03-23 16:25:55 +0300

    Suggestion to @pandunts9 homework 16.4

--- members/pandunts9/HW16/homework4.js
+++ members/pandunts9/HW16/homework4.js
@@ -1,3 +1,3 @@
 let fromStr = require("./pat2")
 
-console.log(fromStr.wordsFromString("$ &&j lp9 g"))
+console.log(fromStr.wordsFromString("$ &&j lp9 g aval$on test"))
--- members/pandunts9/HW16/pat2.js
+++ members/pandunts9/HW16/pat2.js
@@ -1,21 +1,10 @@
 module.exports.wordsFromString = function(str) {
-    let regEx = /[^A-Za-z 0-9]/g
+    let regEx = /[^\w_]/
     let newElem = []
     let arr = str.split(" ")
-    let isSym = false;
     console.log(arr)
     for (let elem of arr) {
-        for(let i = 0;i<elem.length;i++){
-            if(regEx.test(elem[i])){
-                isSym = true
-                i = elem.length
-            }else{
-                isSym = false
-            }
-        }
-        if(isSym == false){
-            newElem.push(elem)
-        }
+        if( !regEx.test( elem ))  newElem.push( elem );
     }    
     return newElem  
 }
