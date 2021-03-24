ba2da73cbc1bec5faeef1b23fc5b56acbcf91206
Tom-chack <tom.controlcenter@gmail.com>
2021-03-24 15:08:42 +0300

    Homework 17.4

---
+++ homework/17/homework4.js
@@ -0,0 +1,8 @@
+//4.Գրել ծրագիր որ աշխատացնելիս  input.txt կկարդա պարունակությունը ,
+// որից հետո այդ պարունակության բոլոր բացատները կփոխարին - ներով:
+// Այնուհետև գծիկներով տեքստ գրել write.txt ֆայլուն
+
+const fs = require('fs');
+let data = fs.readFileSync('./files/input.txt', 'utf-8');
+
+fs.writeFileSync( './files/write.txt', data.replace(/\s/gm, '-') );
