cd740bc40815b3ca61a19db77f83d6051f2c9273
Tom-chack <tom.controlcenter@gmail.com>
2021-04-04 12:44:16 +0300

    Homework 22.5

---
+++ homework/22/homework5.js
@@ -0,0 +1,33 @@
+// 5.Գրել ծրագիր , որը կկարդա homeworks.txt պարունակությունը, 5 պահանջները regexp տեղադրել p թեգերի մեջ և ուղարկել 
+// բրաուրզեր երբ հացում գա server  նաև բրաուզեր ուղարկել script կոդ որը այս 5 p թեգեր մեկ վաjրկայն պարբերությամբ 
+// կսկսեն հայտնվել 1-5 հաջորդականությամբ:
+
+const http = require('http');
+const {readFileSync} = require('fs');
+
+http.createServer( (req, res) => {
+
+    let text = readFileSync('./homeworks.txt', {encoding:'utf8', flag:'r'});
+    let html = text.toString().split('\n\r').map( row => '<p hidden>' + row + '</p>' ).join('\n');
+    
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.write('<!doctype html><html><body><div id="homeworks">' + html + '</div></body></html>');
+    res.end(script());
+
+}).listen(8007);
+
+function script(){
+    return `<script>
+                let elem = 0; 
+                let interval = window.setInterval(() => {
+                    let homeworks = document.getElementById('homeworks');
+                    let elements = homeworks.children;
+                    if( elem >= elements.length ){
+                        window.clearInterval(interval);
+                    } else {
+                        elements[elem].hidden = false;
+                        elem++;
+                    }
+                }, 1000);
+            </script>`;
+}
