44af5ab43c91c652abf590d24458e0de45e75fb3
Tom-chack <tom.controlcenter@gmail.com>
2021-04-11 17:26:10 +0300

    Homework 25.1

---
+++ homework/25/homework1.js
@@ -0,0 +1,9 @@
+//1. Ստեղծել   Datebase  Example անունով:
+
+const {db} = require('./mysql.con.js');
+
+db.query(`CREATE DATABASE IF NOT EXISTS _nodejs`, (e) => {
+    if(e) throw e.message;
+})
+
+db.end();
