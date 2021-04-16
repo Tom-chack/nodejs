686aaee4f1a8b0dde41193833ebf2eb5147b0e1d
Tom-chack <tom.controlcenter@gmail.com>
2021-04-16 14:56:57 +0300

    Homework 27

--- homework/27/index.js
+++ homework/27/index.js
@@ -10,32 +10,24 @@ const {IncomingForm} = require('formidable');
 
 db.connect(config.link, {useUnifiedTopology:true, useNewUrlParser:true})
     .then( result => {
-
         console.log('Connected to MongoDB!');
-
         http.createServer( (req, res) => {
-
             if( req.url == '/save' ){
-
                 let form = new IncomingForm();
-
                 form.parse( req, (e, fields, files ) => {
-
                     const user = new User({
                         name: fields.name,
                         email: fields.email,
                         number: fields.number,
                         message: fields.message
                     });
-
                     user.save()
-                        .then( result => {
-                            res.end( JSON.stringify(result) );
-                        })
-                        .catch( error => {
-                            throw error;
-                        });
-
+                    .then( result => {
+                        res.end( JSON.stringify(result) );
+                    })
+                    .catch( error => {
+                        throw error;
+                    });
                 });
 
             } else {
@@ -43,9 +35,7 @@ db.connect(config.link, {useUnifiedTopology:true, useNewUrlParser:true})
                 res.setHeader('Content-Type', 'text/html');
                 res.end(html);
             }
-
         }).listen(8007);
-
     })
     .catch( error => {
         throw error;
