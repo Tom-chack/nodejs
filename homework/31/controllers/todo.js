abe2c97cad8eb2ba772e1d8a61ecba4171c637c1
Tom-chack <tom.controlcenter@gmail.com>
2021-04-24 01:58:23 +0300

    Working on Homework 31

--- homework/31/controllers/todo.js
+++ homework/31/controllers/todo.js
@@ -4,7 +4,12 @@ const {TODO} = require('../models/todo');
 
 class ToDo {
 
-    async list(req, res){
+    constructor(){
+        this.list = this.list.bind(this);
+        this.add = this.add.bind(this);
+    }
+
+    async list(req, res, message){
 
         try{
             let todos = await TODO.find({}).sort('-updatedAt');
@@ -21,8 +26,18 @@ class ToDo {
 
     }
 
-    add(req, res){
-
+    async add(req, res){
+        let message = '';
+        try{
+            TODO.create({
+                task: req.body.task
+            });
+            message = 'Added Successfully!';
+        } 
+        catch(e) {
+            message = e.message;
+        }
+        this.list(req, res, message);
     }
 
     update(req, res){
--- homework/31/routes/todo.js
+++ homework/31/routes/todo.js
@@ -7,6 +7,8 @@ let router = express.Router();
 
 router.get('/', list);
 
+router.post('/', add);
+
 router.post('/add', add);
 
 router.post('/update', update);
